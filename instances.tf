f3dc3c23caf2956301eaf6653ea1b72df2855ca0
pascal.edouard@brambles.com <pascal.edouard@cloudreach.com>
2022-04-06 17:26:18 +0100

    init commit

---
+++ instances.tf
@@ -0,0 +1,13 @@
+# ATTACH THE INSTANCE PROFILE TO THE PUBLIC INSTANCE
+resource "aws_instance" "web_servers" {
+  count = 4
+  ami                    = data.aws_ami.ubuntu_image.id
+  instance_type          = var.ec2_type
+  subnet_id              = data.aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
+  key_name               = var.my_keypair
+
+  tags = {
+    Name = "ansible_web_server_${count.index + 1}"
+  }
+}
